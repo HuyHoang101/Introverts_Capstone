1672f2dcdf732790547e8bcf37403781ae429f46
nguyenhuyhoang <s3914298@rmit.edu.vn>
2025-05-23 13:24:57 +0700

    push code

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+import express, { json } from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
